b83b561bdb67caf15fb02191c55a8c275c86a9e4
Igor Kulikov <ikulikov@thingsboard.io>
2020-06-02 12:55:53 +0300

    Update static serve config

--- static.serve.conf.js
+++ static.serve.conf.js
@@ -1,9 +1,9 @@
 const STATIC_SERVE_CONFIG = {
-  '/static/rulenode/rulenode-core-config.js': {
-    'target': 'dist/rulenode-core-config/bundles/rulenode-core-config.umd.js'
+  '/static/rulenode/custom-nodes-config.js': {
+    'target': 'dist/custom-nodes-config/bundles/custom-nodes-config.umd.js'
   },
-  '/static/rulenode/rulenode-core-config.umd.js.map': {
-    'target': `dist/rulenode-core-config/bundles/rulenode-core-config.umd.js.map`
+  '/static/rulenode/custom-nodes-config.umd.js.map': {
+    'target': `dist/custom-nodes-config/bundles/custom-nodes-config.umd.js.map`
   }
 }
 
